429b974b9a0a2ba864730961b5452fa02ca33366
r0pchainz <38400850+r0pchainz@users.noreply.github.com>
2018-06-14 15:45:18 -0400

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,16 @@
+sudo apt-get install 
+     apt-transport-https \
+     apt-transport-tor \
+     python3-pip \
+     python-pip \
+     torchat \
+     qemu \
+     qemu-system \
+     virt-manager \
+     virtualbox \
+     deluge \
+     docker-ce \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
